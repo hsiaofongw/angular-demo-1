23a41025d80b4825aab61269a5f893f55ff218dc
hsiaofongw <hsiaofong.w@gmail.com>
2021-06-26 18:56:28 +0800

    feat: 添加了 Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
