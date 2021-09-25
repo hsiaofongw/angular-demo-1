b4703c43f68aa8701676019d731f96a94f288830
hsiaofongw <hsiaofong.w@gmail.com>
2021-09-25 13:03:22 +0800

    feat: 完善日志记录工具

--- src/app/article/interface.ts
+++ src/app/article/interface.ts
@@ -10,5 +10,8 @@ import {
 } from 'src/types/interfaces';
 export { Page };
 
+/** 文章列表分页请求参数 */
 export type ArticleListQueryParameter = OffsetLimitQueryParameter;
+
+/** 文字列表分页请求结果 */
 export type ArticleListQueryResult = PaginationQueryResult<IArticle>;
--- src/app/shared-modules/logging/services/logger.service.ts
+++ src/app/shared-modules/logging/services/logger.service.ts
@@ -1,4 +1,15 @@
-import { LoggerConfig, Serializable } from '../interfaces';
+import {
+  LoggerConfig,
+  Serializable,
+  StandardLog,
+  StandardLogLevel,
+} from '../interfaces';
+
+/** 未定级日志 */
+interface IUnLeveledLog {
+  /** 记录日志为给定级别 */
+  logAs(level: StandardLogLevel): void;
+}
 
 /** 日志记录器 */
 export class Logger {
@@ -9,20 +20,44 @@ export class Logger {
     return new Date().valueOf();
   }
 
-  /** 记录 info 级的日志 */
-  public info<T extends Serializable>(content: T): void {
-    this.config.recorder.next({
+  /** 创建未定级日志 */
+  private _makeUnLeveledLog<T extends Serializable>(content: T): IUnLeveledLog {
+    const recorder = this.config.recorder;
+
+    const unLeveledLog: Omit<StandardLog, 'level'> = {
       datetime: this._getNow(),
       moduleId: this.config.moduleId,
       classId: this.config.classId,
       content: content,
-      level: 'info',
-    });
+    };
+
+    class UnLeveledLog {
+      logAs(level: StandardLogLevel): void {
+        const fullLog: StandardLog = { ...unLeveledLog, level };
+        recorder.next(fullLog);
+      }
+    }
+
+    return new UnLeveledLog();
   }
 
-  public error(content: string): void {}
+  /** 记录 info 级的日志 */
+  public info<T extends Serializable>(content: T): void {
+    this._makeUnLeveledLog(content).logAs('info');
+  }
 
-  public warning(content: string): void {}
+  /** 记录 error 级的日志 */
+  public error(content: string): void {
+    this._makeUnLeveledLog(content).logAs('error');
+  }
 
-  public debug(content: string): void {}
+  /** 记录 warning 级的日志 */
+  public warning(content: string): void {
+    this._makeUnLeveledLog(content).logAs('warning');
+  }
+
+  /** 记录 debug 级的日志 */
+  public debug(content: string): void {
+    this._makeUnLeveledLog(content).logAs('debug');
+  }
 }
